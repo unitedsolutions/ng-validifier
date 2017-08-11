03110e260a8728ab684af328861fda2a78718991
aptivator <aptivator@gmail.com>
2017-08-11 12:03:15 -0400

    Spin off module generator from configurator

--- src/configurator/configurator.ts
+++ src/configurator/configurator.ts
@@ -1,28 +1,10 @@
 import * as _                     from 'lodash';
-import {NgModule}                 from '@angular/core';
-import internalValidators         from '../validators/validators';
-import prefixer                   from './_lib/prefixer';
 import {settings as mainSettings} from '../_lib/vars';
-import validatorConfigurator      from './validator-configurator/validator-configurator';
-import directivesGenerator        from './directives-generator/directives-generator';
+import internalValidators         from '../validators/validators';
+import moduleGenerator            from './module-generator/module-generator';
 
 export default (settings, validators = []) => {
   settings = _.extend({}, mainSettings, settings);
-  validators = internalValidators.concat(validators);
-  validators = validators.map(validator => {
-    let {validatorName, directiveName} = validator;
-    let validatorNamePrefixed = prefixer(settings, validatorName);
-    let directiveNamePrefixed = prefixer(settings, directiveName);
-    _.extend(validator, {validatorNamePrefixed, directiveNamePrefixed});
-    return validatorConfigurator(settings, validator);
-  });
-  
-  let directives = directivesGenerator(validators);
-  
-  @NgModule({
-    declarations: directives, 
-    exports: directives
-  }) class ValidatorModule {}
-  
-  return ValidatorModule;
+  validators.push(...internalValidators);
+  return moduleGenerator(settings, validators);
 };
---
+++ src/configurator/module-generator/module-generator.ts
@@ -0,0 +1,24 @@
+import * as _                from 'lodash';
+import {NgModule}            from '@angular/core';
+import prefixer              from '../_lib/prefixer';
+import validatorConfigurator from '../validator-configurator/validator-configurator';
+import directivesGenerator   from '../directives-generator/directives-generator';
+
+export default (settings, validators) => {
+  validators = validators.map(validator => {
+    let {validatorName, directiveName} = validator;
+    let validatorNamePrefixed = prefixer(settings, validatorName);
+    let directiveNamePrefixed = prefixer(settings, directiveName);
+    _.extend(validator, {validatorNamePrefixed, directiveNamePrefixed});
+    return validatorConfigurator(settings, validator);
+  });
+  
+  let directives = directivesGenerator(validators);
+
+  @NgModule({
+    declarations: directives, 
+    exports: directives
+  }) class ValidatorModule {}
+  
+  return ValidatorModule;    
+};
