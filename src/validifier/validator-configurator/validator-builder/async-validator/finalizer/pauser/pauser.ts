5eb322e4107f8f5b2b3718e0461d9c53bc8cd3d0
aptivator <aptivator@gmail.com>
2017-10-09 17:37:03 -0400

    Test pauser

--- src/validifier/validator-configurator/validator-builder/async-validator/finalizer/pauser/pauser.ts
+++ src/validifier/validator-configurator/validator-builder/async-validator/finalizer/pauser/pauser.ts
@@ -4,6 +4,8 @@ export default (time, settings) => {
   time = _.now() - time;
   time = (settings.asyncMinimumPendingDuration || time) - time;
   
+  console.log(time, settings);
+  
   if(time > 0) {
     time = 0;
   }
